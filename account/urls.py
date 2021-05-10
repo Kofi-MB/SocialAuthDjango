9dfc42c4b78af5569bebb06915917ed62ee606af
Binay kumar Pradhan <67338124+pbinaykumar@users.noreply.github.com>
2021-05-10 12:54:34 +0530

    'complete'

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('get-user',views.get_user)
+    path('get-user',views.get_user),
+    path('send-sms',views.send_sms)
 ]
--- account/views.py
+++ account/views.py
@@ -2,7 +2,8 @@ from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from oauth2_provider.models import AccessToken
 from django.http import JsonResponse
-
+import requests
+import json
 
 
 @csrf_exempt
@@ -11,3 +12,25 @@ def get_user(request):
     user_id=AccessToken.objects.get(token=access_token).user_id
     user=list(User.objects.filter(id=user_id).values())
     return JsonResponse(user,safe=False)
+
+@csrf_exempt
+def send_sms(request):
+    headers = {
+        'authorization': 'tY3qVKk2G4Ol8srpoQ5RLagdAfCwXehNEzx17UPJFiWHTZM9bmi6JojuLUkWIwYfO4hZ3QP9rKmTDpal',
+    }
+    data = {
+        'sender_id': "TXTIND",
+        'message': 'lore, ipsium kwhkvfkwb wkbf wm fkw  fekbiwb flwi fec ibwfgweefiw fwh hf9  hg fbwf8o3t4r3 99r 93t3ihh h3hh9p3hyt 3p98h ihh ho h2hih wbf bwg jiwah fqwljbfiiwbk;f',
+        'language': 'english',
+        'route': 'v3',
+        'numbers': '9178595277'
+    }
+
+    response = requests.post('https://www.fast2sms.com/dev/bulkV2', headers=headers, data=data)
+    messageResponse = json.loads(response.text)
+    status=messageResponse['message']
+    balanceResponse = requests.request("GET",
+                                       url="https://www.fast2sms.com/dev/wallet?authorization=tY3qVKk2G4Ol8srpoQ5RLagdAfCwXehNEzx17UPJFiWHTZM9bmi6JojuLUkWIwYfO4hZ3QP9rKmTDpal")
+    balance=json.loads(balanceResponse.text)
+    result={'status':status,'balance':balance}
+    return JsonResponse(result,safe=False)
