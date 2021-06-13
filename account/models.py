cd337f26e79093cfb51b1d1966de6278ddb4be51
Binay kumar Pradhan <67338124+pbinaykumar@users.noreply.github.com>
2021-06-13 22:58:14 +0530

    'set'

--- account/models.py
+++ account/models.py
@@ -1,9 +1,18 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
+class CustomAuth(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    mobile_number=models.CharField(max_length=12)
+
+
+
 class CompanyDetail(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     companyname=models.CharField(max_length=50)
 
     def __str__(self):
         return str(self.user)
+
+
