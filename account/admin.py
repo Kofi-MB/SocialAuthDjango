422dce9afe7646c81e3995cdc4f6bafb8d30ea5b
Binay kumar Pradhan <67338124+pbinaykumar@users.noreply.github.com>
2021-06-13 23:00:11 +0530

    'set'

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import CompanyDetail
+from .models import CompanyDetail,CustomAuth
 
 admin.site.register(CompanyDetail)
+admin.site.register(CustomAuth)
